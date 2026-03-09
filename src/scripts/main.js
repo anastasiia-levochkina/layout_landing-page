263ae2c7607714a2bdbd7021d13d2d85d1bba6c5
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2026-03-10 00:42:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-'use strict';
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const closeBtn = document.querySelector('.menu__close');
+
+menuBtn.onclick = () => {
+  menu.classList.add('active');
+};
+
+closeBtn.onclick = () => {
+  menu.classList.remove('active');
+};
